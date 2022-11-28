a74f4113f03522f21c0dc17a84a43b39e6be320d
Guilherme Garcia <71122284+VoidGS@users.noreply.github.com>
2022-11-28 19:42:45 -0300

    Config defaultRoute

--- config/web.php
+++ config/web.php
@@ -7,6 +7,7 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'defaultRoute' => '/site/home',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
